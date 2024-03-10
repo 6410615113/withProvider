a6803107537c0db652bfeaeef7ed2233e15f03f9
Pitchakan Leephud <6410615113@cn103>
2024-03-10 11:42:00 +0700

    init

--- django-social-auth/.venv/Scripts/activate.bat
+++ django-social-auth/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\cn\year3\3-2\cn332\django-social-auth\.venv
+set VIRTUAL_ENV=D:\cn\year3\3-2\cn332\withProvider\django-social-auth\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
